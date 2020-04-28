ce602eb1e9b1272867edbee58b5b3a7d5f8570b4
John Loberger <johnloberger@gmail.com>
2020-04-28 21:18:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,39 +1,20 @@
 def nyc_pigeon_organizer(data)
-  {
-    "Theo" => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Subway"]
-  },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
-  },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  },
-  "Ms. K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
-  },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
-  },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
-  },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  }
-}
+  hash = {}
+  data.each do |key, value|
+    value.each do |new_val, names|
+      names.each do |name|
+        
+        if !hash[name]
+          hash[name] = {}
+        end
+
+        if !hash[name][key]
+          hash[name][key] = {}
+        end
+
+        hash[name][key] << new_val.to_s
+      end
+    end
+  end
+  hash  
 end
